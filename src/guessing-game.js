525ff749891c9be89f6db55a95b3ac9826fdcb0a
ElenaKilljoy <lenokcardio@gmail.com>
2023-02-08 11:19:29 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.root = null;
+    }
 
     setRange(min, max) {
-
+        this.left = min;     
+        this.right = max;
     }
 
     guess() {
-
+        if (this.left === 0) {    
+            return Math.round(this.right / 2);
+        } else {    
+            return Math.round((this.right - this.left) / 2) + this.left;
+        }
     }
 
     lower() {
-
+        this.right = this.guess();
     }
 
     greater() {
-
+        this.left = this.guess();
     }
 }
 
